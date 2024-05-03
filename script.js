a3b3dc34a14ccf05d9be2c80d2bc902cee136e62
atreska <115714570+atreska@users.noreply.github.com>
2024-05-02 22:18:54 -0400

    Clean up code and create function for button event listener

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let button = document.querySelector('.submit');
+let submitButton = document.querySelector('.submit');
 let span = document.querySelector('.selection');
+let radioButton = document.querySelectorAll('input');
 let radioButtonValue = document.getElementsByName('radio');
+let thankYouState = document.querySelector('.thank-you-state');
+let ratingComponent = document.querySelector('.rating-component');
 
+for (btn of radioButton) {
+    btn.addEventListener('click', (e) => {
+        submitButton.disabled = false;
+    })
+}
 
-button.addEventListener('click', (e) => {
+function submitRating(e) {
+    //prevent the page from loading to keep the selected value passed to
+    //the span tag
     e.preventDefault();
     let selectionValue = '';
 
@@ -13,4 +23,10 @@ button.addEventListener('click', (e) => {
         }
     }
     span.textContent = selectionValue;
-})
+    ratingComponent.style.display = 'none';
+    //use below instead of display block while making the thank-you-state visible
+    //to avoid adding extra margin/whitespace
+    thankYouState.style.display = 'block flex';
+}
+
+submitButton.addEventListener('click', submitRating);
